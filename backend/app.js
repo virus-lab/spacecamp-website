eb3b095d2d83bb433d1031e39f0326ab202ba411
PJunhyuk-mac <jgravity@naver.com>
2018-05-11 00:44:21 +0900

    Remove # in url

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(require('connect-history-api-fallback')());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

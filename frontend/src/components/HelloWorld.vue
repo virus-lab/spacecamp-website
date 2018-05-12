cae498ca5ca71801f10c325e0f8b459b7c3d3674
PJunhyuk <jgravity@naver.com>
2018-05-12 16:25:38 +0900

    Add counter

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,21 +1,37 @@
 <template>
   <div class="hello">
-    <div id="main-typo">
+    <div id="main-typo" class="main-font">
       <h1 lang="ko">우주기술</h1>
       <h1 lang="ko">대학생</h1>
       <h1 lang="ko">창업 아카데미</h1>
       <h1 lang="en" class="font-color_1">STAR-EXPLORATION</h1>
     </div>
+    <div id="main-day" class="main-font">
+      <h2 id="main-target" class="font-color_1" lang="en">2018. 07. 16</h2>
+      <h2 id="main-countdown" lang="en">
+        <countdown :time="time" :interval="100">
+          <template slot-scope="props">{{ props.days }}:{{ props.hours }}:{{ props.minutes }}:{{ props.seconds.split('.')[0] }}</template>
+        </countdown>
+      </h2>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
   data () {
+    var now = new Date()
+    var newYear = new Date(2018, 7, 16)
+
     return {
-      msg: 'Welcome to Your Vue.js App'
+      counting: false,
+      time: newYear - now
     }
+  },
+  mounted () {
+    // this.$refs.countdown.start()
   }
 }
 </script>
@@ -59,13 +75,33 @@ export default {
 }
 
 /* for main-typo */
-#main-typo {
-  font-size: 48px;
+.main-font {
+  font-size: 60px;
   font-weight: 800;
   color: white;
-  padding-left: 50px;
-  padding-top: 50px;
   line-height: 1.3em;
 }
 
+#main-typo {
+  padding-left: 50px;
+  padding-top: 50px;
+}
+
+#main-day {
+  position: absolute;
+  bottom: 0px;
+  right: 0px;
+  padding-right: 0px;
+  padding-bottom: 50px;
+}
+
+#main-target {
+  width: 400px;
+  text-align: justify;
+}
+
+#main-countdown {
+  width: 400px;
+  text-align: justify;
+}
 </style>

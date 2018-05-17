c9d21091330e4864241e431149b2816791529e14
PJunhyuk-mac <jgravity@naver.com>
2018-05-18 02:45:01 +0900

    Add fonts and button in index

--- frontend/src/components/index.vue
+++ frontend/src/components/index.vue
@@ -2,8 +2,16 @@
   <div class="main">
 
     <b-jumbotron class="fullpage" id="fullpage-main">
-      <p>For more information visit website</p>
-      <b-btn variant="primary" href="#">More Info</b-btn>
+
+      <div id="main-typo">
+        <h1 lang="ko" class="font-color_1 font-bold">우주기술</h1>
+        <h1 lang="ko" class="font-color_1 font-bold">대학생</h1>
+        <h1 lang="ko" class="font-color_1 font-bold">창업아카데미</h1>
+        <h1 lang="en" class="font-color_2 font-bold">STAR-EXPLORATION</h1>
+      </div>
+
+      <a id="btn-more" lang="ko" class="font-color_1 font-bold" href="#">더 알아보기</a>
+
     </b-jumbotron>
 
     <affix id="menu" relative-element-selector="#main-contents" style="width: 100%" :offset="{ top: 56, bottom: 56 }" :scroll-affix="true" v-on:affix="navAttach" v-on:affixtop="navDetach">
@@ -81,6 +89,45 @@ export default {
 
 /* end default */
 
+/* start font */
+
+@import url(http://fonts.googleapis.com/earlyaccess/notosanskr.css);
+
+@font-face {
+  font-family: 'campton-bolddemo';
+  src: url('./../assets/campton-bolddemo-webfont.woff2') format('woff2'),
+  url('./../assets/campton-bolddemo-webfont.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'campton-lightdemo';
+  src: url('./../assets/campton-lightdemo-webfont.woff2') format('woff2'),
+  url('./../assets/campton-lightdemo-webfont.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+
+.font-bold:lang(ko) {
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight: 800;
+}
+
+.font-bold:lang(en) {
+  font-family: 'campton-bolddemo', sans-serif;
+}
+
+.font-color_1 {
+  color: white;
+}
+
+.font-color_2 {
+  color: #00fcff;
+}
+
+/* end font */
+
 /* start fullpage-main */
 
 .fullpage {
@@ -92,6 +139,23 @@ export default {
 }
 #fullpage-main {
   background-image: url("./../assets/bg_space.jpg");
+}
+
+#btn-more {
+  position: absolute;
+  left: 50%;
+  width: 300px;
+  margin-left: -150px;
+  text-align: center;
+  top: 50%;
+  vertical-align: center;
+  padding: 10px;
+  border: 8px solid white;
+  font-size: 36px;
+}
+#btn-more:hover {
+  border: 8px solid #00fcff;
+  color: #00fcff;
 }
 
 /* end fullpage-main */

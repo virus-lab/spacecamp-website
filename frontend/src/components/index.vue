48ad43509ea68cf5762f199cec3907dddb778743
PJunhyuk-mac <jgravity@naver.com>
2018-05-18 02:27:11 +0900

    Apply jumbotron and sticky-nav in index

--- frontend/src/components/index.vue
+++ frontend/src/components/index.vue
@@ -1,85 +1,53 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="main">
+
+    <b-jumbotron class="fullpage" id="fullpage-main">
+      <p>For more information visit website</p>
+      <b-btn variant="primary" href="#">More Info</b-btn>
+    </b-jumbotron>
+
+    <affix id="menu" relative-element-selector="#main-contents" style="width: 100%" :offset="{ top: 56, bottom: 56 }" :scroll-affix="true" v-on:affix="navAttach" v-on:affixtop="navDetach">
+      <b-navbar toggleable="md" type="dark" variant="info" id="nav" class="stickyed">
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+        <b-collapse is-nav id="nav_collapse">
+          <b-navbar-nav>
+            <b-nav-item href="#1">Link</b-nav-item>
+            <b-nav-item href="#2">Link</b-nav-item>
+            <b-nav-item href="#3">Link</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </affix>
+
+    <div id="main-contents">
+
+      <div v-bind:class="[{ active: isNavTouch }]" class="noactive">
+      </div>
+
+      <b-jumbotron class="fullpage">
+        <p>For more information visit website</p>
+        <b-btn variant="primary" href="#">More Info</b-btn>
+      </b-jumbotron>
+      <b-jumbotron class="fullpage">
+        <p>For more information visit website</p>
+        <b-btn variant="primary" href="#">More Info</b-btn>
+      </b-jumbotron>
+      <b-jumbotron class="fullpage">
+        <p>For more information visit website</p>
+        <b-btn variant="primary" href="#">More Info</b-btn>
+      </b-jumbotron>
+      <b-jumbotron class="fullpage">
+        <p>For more information visit website</p>
+        <b-btn variant="primary" href="#">More Info</b-btn>
+      </b-jumbotron>
+      <b-jumbotron class="fullpage">
+        <p>For more information visit website</p>
+        <b-btn variant="primary" href="#">More Info</b-btn>
+      </b-jumbotron>
+
+    </div>
+
   </div>
 </template>
 
@@ -88,7 +56,15 @@ export default {
   name: 'index',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      isNavTouch: false
+    }
+  },
+  methods: {
+    navAttach: function (event) {
+      this.isNavTouch = true
+    },
+    navDetach: function (event) {
+      this.isNavTouch = false
     }
   }
 }
@@ -96,18 +72,45 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+
+/* start default */
+
+.main {
+  height: 100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+/* end default */
+
+/* start fullpage-main */
+
+.fullpage {
+  margin: 0px;
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+#fullpage-main {
+  background-image: url("./../assets/bg_space.jpg");
 }
-a {
-  color: #42b983;
+
+/* end fullpage-main */
+
+/* start nav */
+
+.noactive {
+  background-color: white;
+  height: 500px;
 }
+
+.active {
+  height: 556px;
+}
+
+/* end nav */
+
+#main-contents {
+  padding-top: 50px;
+}
+
 </style>

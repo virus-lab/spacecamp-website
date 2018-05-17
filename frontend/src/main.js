25c9f425e229c5b4f4cee7a05ebad9c2dd628eb0
PJunhyuk-mac <jgravity@naver.com>
2018-05-18 02:05:34 +0900

    Install BootstrapVue and VueAffix

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import VueAffix from 'vue-affix'
+
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue);
+
+Vue.use(VueAffix);
 
 /* eslint-disable no-new */
 new Vue({
